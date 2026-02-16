d2a22ee7693e959190c5d80b9838165ffe0dee47
Jerald Lim <jeraldssw@gmail.com>
2026-02-16 17:17:08 +0800

    perf(backend): allow all origins

--- backend/app.py
+++ backend/app.py
@@ -121,7 +121,7 @@ def create_app() -> FastAPI:
     app = FastAPI(root_path="/api")
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000"],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
