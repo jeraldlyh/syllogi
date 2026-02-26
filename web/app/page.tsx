7a8f966ff890ab1bfbb1fa60012f9539ec9b15ec
Jerald Lim <jeraldssw@gmail.com>
2026-02-26 21:46:18 +0800

    style(web): change grid format to single flex column page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { DashboardHeader } from "@/components/dashboard-header";
+import { Playlists } from "@/components/playlist";
 import { SyncSummary } from "@/components/sync-summary";
 import { SyncTable } from "@/components/sync-table";
 
@@ -9,12 +10,11 @@ export default function Page() {
     <div className="min-h-screen bg-background">
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <DashboardHeader />
-        <div className="mt-6 grid grid-cols-1 gap-6 lg:grid-cols-3">
-          <main className="flex flex-col gap-6 lg:col-span-2">
-            <SyncSummary />
-            <SyncTable />
-          </main>
-        </div>
+        <main className="mt-14 flex flex-col gap-6">
+          <SyncSummary />
+          <Playlists />
+          <SyncTable />
+        </main>
       </div>
     </div>
   );
