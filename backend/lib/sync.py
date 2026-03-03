62aa2f9a9473ce0c598ac5e7fb09baf17f041955
Jerald Lim <jeraldssw@gmail.com>
2026-03-03 11:31:10 +0800

    fix(backend): mismatched values for switch case

--- backend/lib/sync.py
+++ backend/lib/sync.py
@@ -1,11 +1,11 @@
-from db.models.playlist import Playlist, PlaylistProvider
+from db.models.playlist import Playlist
 from db.session import SessionDep
 from lib.spotify import _sync_spotify_playlist
 
 
 def _sync_playlist(playlist: Playlist, session: SessionDep) -> dict[str, str]:
     match playlist.provider:
-        case PlaylistProvider.spotify:
+        case "spotify":
             return _sync_spotify_playlist(item=playlist, session=session)
         case _:
             return {}
