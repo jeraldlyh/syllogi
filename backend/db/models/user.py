c229d3d02ec0b0c65a6c4958690c45870bb014a3
Jerald Lim <jeraldssw@gmail.com>
2026-04-06 22:23:03 +0800

    perf(backend): set username to be unique and create index

--- backend/db/models/user.py
+++ backend/db/models/user.py
@@ -6,5 +6,5 @@ from sqlmodel import Field, SQLModel
 
 class User(TimestampMixin, SerializerMixin, SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, nullable=False)
-    username: str = Field(max_length=128, nullable=False)
+    username: str = Field(max_length=128, nullable=False, unique=True, index=True)
     password: str = Field(max_length=256, nullable=False)
