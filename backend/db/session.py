7bac2f9653dcabfc6224efe01aa74be32e762ba7
Jerald Lim <jeraldssw@gmail.com>
2026-05-16 18:23:42 +0800

    fix(backend): incorrect environment variable for database username

--- backend/db/session.py
+++ backend/db/session.py
@@ -7,7 +7,7 @@ from lib.env import get_environment_variable
 
 
 def get_connection_string() -> str:
-    username = get_environment_variable("DATABASE_NAME")
+    username = get_environment_variable("DATABASE_USERNAME")
     password = get_environment_variable("DATABASE_PASSWORD")
     url = get_environment_variable("DATABASE_URL")
     name = get_environment_variable("DATABASE_NAME")
