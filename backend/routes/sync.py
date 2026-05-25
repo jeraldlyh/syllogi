5b0bc00bd7117b0b2c644a73972eea06880cfc25
Jerald Lim <jeraldssw@gmail.com>
2026-05-25 10:09:06 +0800

    fix(backend): session handling

--- backend/routes/sync.py
+++ backend/routes/sync.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, BackgroundTasks, HTTPException, status
 from db.models.playlist import Playlist, PlaylistProvider
 from db.models.sync_session import SyncProvider, SyncSession, SyncStatus
 from db.playlist import get_playlist_by_id
-from db.session import get_isolated_session
+from db.session import SessionDep
 from db.sync_session import create_sync_session
 from lib.models.common import ExternalPlaylist, ExternalTrack
 from lib.sync import sync_playlist_task
@@ -56,7 +56,9 @@ logger = logging.getLogger(__name__)
         },
     },
 )
-def sync_playlist(item: Playlist, background_tasks: BackgroundTasks) -> dict[str, str]:
+def sync_playlist(
+    item: Playlist, background_tasks: BackgroundTasks, session: SessionDep
+) -> dict[str, str]:
     songs: list[ExternalTrack] = []
     external_playlist: ExternalPlaylist | None = None
 
@@ -68,33 +70,32 @@ def sync_playlist(item: Playlist, background_tasks: BackgroundTasks) -> dict[str
             songs = get_youtube_playlist_songs(playlist_id=item.playlist_id)
             external_playlist = get_youtube_playlist(playlist_id=item.playlist_id)
 
-    with get_isolated_session() as session:
-        internal_playlist = get_playlist_by_id(session=session, playlist_id=item.id)
+    internal_playlist = get_playlist_by_id(session=session, playlist_id=item.id)
 
-        if not internal_playlist:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Unable to find playlist: {item.playlist_id}",
-            )
-
-        playlist_id = internal_playlist.playlist_id
-        username = item.username
-        started_at = get_now()
-
-        sync_session = SyncSession(
-            provider=SyncProvider(internal_playlist.provider.value),
-            provider_playlist_id=playlist_id,
-            provider_playlist_name="",
-            target_user_id="",
-            target_username=username,
-            target_playlist_id="",
-            target_playlist_name="",
-            started_at=started_at,
-            finished_at=started_at,
-            duration_seconds=0,
-            status=SyncStatus.pending,
+    if not internal_playlist or not external_playlist:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Unable to find playlist: {item.playlist_id}",
         )
-        create_sync_session(session=session, sync_session=sync_session)
+
+    playlist_id = internal_playlist.playlist_id
+    username = item.username
+    started_at = get_now()
+
+    sync_session = SyncSession(
+        provider=SyncProvider(internal_playlist.provider.value),
+        provider_playlist_id=playlist_id,
+        provider_playlist_name="",
+        target_user_id="",
+        target_username=username,
+        target_playlist_id="",
+        target_playlist_name="",
+        started_at=started_at,
+        finished_at=started_at,
+        duration_seconds=0,
+        status=SyncStatus.pending,
+    )
+    create_sync_session(session=session, sync_session=sync_session)
 
     background_tasks.add_task(
         sync_playlist_task,
